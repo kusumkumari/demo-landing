38df8c170fca01f5bbe4233876df1b889066539d
kusumkumari <kusumk2992@gmail.com>
2021-01-26 16:45:58 +0530

    setup project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { Route,Link,Switch} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Nav from './Nav'
 import Home from './Pages/Home'
 import About from './Pages/About'
@@ -12,13 +11,13 @@ function App() {
   return (
     <div className="App">
       <h1> Router Management </h1>
-          <Nav />
-          <Switch>
-            <Route path='/about'>    <About /></Route>
-            <Route path='/contact'>  <Contact /> </Route>
-                        <Route path='/' exact={true}>     <Home /> </Route>
-            <Route path='*'>     <PageNotFound /> </Route>
-          </Switch>
+      <Nav />
+      <Switch>
+        <Route path='/about'>    <About /></Route>
+        <Route path='/contact'>  <Contact /> </Route>
+        <Route path='/' exact={true}>     <Home /> </Route>
+        <Route path='*'>     <PageNotFound /> </Route>
+      </Switch>
     </div>
   );
 }
